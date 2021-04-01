35b1641a7f8abf4fd43245c4807537b8d6433e42
Zane Fink <finkzane@gmail.com>
2021-04-01 14:51:39 -0500

    Add hittable class, list of hittables, aggregate common utilities

---
+++ src/common.h
@@ -0,0 +1,25 @@
+#ifndef COMMON_HH_INCLUDED
+#define COMMON_HH_INCLUDED
+
+#include <cmath>
+#include <limits>
+#include <memory>
+
+
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+inline double degrees_to_radians(double degrees)
+{
+  return degrees * pi / 180.0;
+}
+
+#include "ray.h"
+#include "vec3.h"
+
+#endif // COMMON_HH_INCLUDED
---
+++ src/hittable.h
@@ -0,0 +1,29 @@
+#ifndef HITTABLE_HH_INCLUDED
+#define HITTABLE_HH_INCLUDED
+#include "ray.h"
+
+struct hit_record
+{
+  point3 p;
+  // surface norm of the hit
+  vec3 normal;
+  double t;
+  bool front_face;
+
+  inline void set_face_normal(const ray& r, const vec3& outward_normal)
+  {
+    front_face = dot(r.direction(), outward_normal) < 0;
+    normal = front_face ? outward_normal : -outward_normal;
+  }
+};
+
+class hittable
+{
+ public:
+  // Is there a t_min < t < t_max such that ray's P(t) intersects with the object?
+  virtual bool hit(const ray&r, double t_min, double t_max, hit_record& rec) const = 0;
+
+};
+
+
+#endif // HITTABLE_HH_INCLUDED
