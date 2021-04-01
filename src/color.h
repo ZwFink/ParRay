084966e8b84877b2230d6814e24d6644e1860dd6
Zane Fink <finkzane@gmail.com>
2021-04-01 16:56:37 -0500

    write_pixel now uses value for samples_per_pixel

--- src/color.h
+++ src/color.h
@@ -2,14 +2,24 @@
 #define COLOR_HH_INCLUDED
 
 #include "vec3.h"
+#include "common.h"
 #include <iostream>
 
-void write_color(std::ostream &out, color pixel_color)
+void write_color(std::ostream &out, color pixel_color, int samples_per_pixel)
 {
-  out << static_cast<int>(255.999 * pixel_color.x()) << ' '
-      << static_cast<int>(255.999 * pixel_color.y()) << ' '
-      << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+  auto r = pixel_color.x();
+  auto g = pixel_color.y();
+  auto b = pixel_color.z();
 
+  auto scale = 1.0 / samples_per_pixel;
+  r *= scale;
+  g *= scale;
+  b *= scale;
+
+  out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+      << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+      << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
 }
+
 
 #endif // color_hh_included
