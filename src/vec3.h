2f283de8d9ce3a950f69eb181365d5afb02174e9
Zane Fink <finkzane@gmail.com>
2021-04-06 14:47:13 -0500

    Add final scene

--- src/vec3.h
+++ src/vec3.h
@@ -54,7 +54,7 @@ class vec3
     return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
   }
 
-  inline static vec3 rand()
+  inline static vec3 random()
   {
     return vec3(random_double(), random_double(), random_double());
   }
