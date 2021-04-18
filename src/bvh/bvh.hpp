dc95b0c346f8df1f4a7055780fc67cb3d0ac71f4
Harry Qiu <cjf12harry@gmail.com>
2021-04-18 19:47:12 -0400

    resolve merge conflict

--- src/bvh/bvh.hpp
+++ src/bvh/bvh.hpp
@@ -1,90 +1,20 @@
 #ifndef __H_BVH
 #define __H_BVH
-<<<<<<< HEAD
 
 #include "hittable.h"
 #include <float.h>
 
-const int kNumPlaneSetNormals=7;
+const int kNumPlaneSetNormals = 7;
 
 class Extent
 {
-    public:
+public:
     Extent();
     void extendBy(const Extent &extents);
     bool interset(const double *numberator, const double *denominator, double &tNear, double &tFar, int &planeIndex);
 
-    private:
+private:
     double d[kNumPlaneSetNormals][2]; //the distance d values for each plane set normals
-    hittable *object;             //pointer contained by the volumn
-
+    hittable *object;                 //pointer contained by the volumn
 };
-
-
-=======
-
-#include "vec3.h"
-#include "hittable.h"
-#include "float.h"
->>>>>>> b5b6790fbfea68c862b10150d1a1fd558267835f
-
-extern const int kNumPlaneSetNormals=7;
-
-class Extent
-{
-    public:
-    Extent();
-    void extendBy(const Extent &extents);
-    bool interset(const double *numberator, const double *denominator, double &tNear, double &tFar, int &planeIndex);
-
-    private:
-    double d[kNumPlaneSetNormals][2]; //the distance d values for each plane set normals
-    hittable *object;             //pointer contained by the volumn
-
-};
-
-
-
-/*
-class bvh
-{
-    static const uint8_t kNumPlaneSetNormals = 7;
-    static const vec3 planeSetNormals[kNumPlaneSetNormals];
-    struct Extents
-    {
-        Extents()
-        {
-            for (uint8_t i = 0; i < kNumPlaneSetNormals; ++i)
-            {
-                d[i][0] = DBL_MAX;
-                d[i][1] = -DBL_MAX;
-            }
-        }
-        void extendBy(const Extents &extents)
-        {
-            for (int i = 0; i < kNumPlaneSetNormals; ++i)
-            {
-                if (extents.d[i][0] < d[i][0])
-                    d[i][0] = extents.d[i][0];
-                if (extents.d[i][1] > d[i][1])
-                    d[i][1] = extents.d[i][1];
-            }
-        }
-        bool interset(const double *numberator, const double denominator, double &tNear, double &tFar, int &planeIndex);
-        double d[kNumPlaneSetNormals][2]; //the distance d values for each plane set normals
-        const Object *object;             //pointer contained by the volumn
-    };
-    Extents *
-} public : bvh();
-~bvh();
-}
-;
-bvh::bvh()
-{
-}
-
-bvh::~bvh()
-{
-}
-*/
 #endif
