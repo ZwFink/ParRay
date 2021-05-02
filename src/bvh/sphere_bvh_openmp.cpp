8fbc2906597d7539133d993a45109fdd3a41c4e4
Harry Qiu <cjf12harry@gmail.com>
2021-05-02 16:16:25 -0400

    use default camera

--- src/bvh/sphere_bvh_openmp.cpp
+++ src/bvh/sphere_bvh_openmp.cpp
@@ -22,25 +22,17 @@ int main(int argc, char** argv)
   int num_threads = std::atoi(argv[2]);
   std::cerr << "Rendering scene " << sceneFile << " using " << num_threads << " threads\n";
 
+    camera cam = camera::getDefault();
     // Image
-    const auto aspect_ratio = 3.0 / 2.0;
     const int image_width = 1200;
-    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int image_height = static_cast<int>(image_width / cam.aspect_ratio);
     const int samples_per_pixel = 500;
     const int max_depth = 10;
 
-  // World
-  BVH world(scene_spheres);
-  point3 lookfrom(13, 2, 3);
-  point3 lookat(0, 0, 0);
-  vec3 vup(0, 1, 0);
-  auto dist_to_focus = 10.0;
-  auto aperture = 0.1;
-  camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
-
-  traceConfig config(cam, image_width, image_height, max_depth, samples_per_pixel, num_threads, 0, 1);
-
-  raytracing_bvh(config, world);
-  std::cerr << "\nDone.\n";
-  shapeIO.clear_scene(scene_spheres);
+    // World
+    BVH world(scene_spheres);
+    traceConfig config(cam, image_width, image_height, max_depth, samples_per_pixel, num_threads, 0, 1);
+    raytracing_bvh(config, world);
+    std::cerr << "\nDone.\n";
+    shapeIO.clear_scene(scene_spheres);
 }
--- src/bvh/sphere_bvh_single.cpp
+++ src/bvh/sphere_bvh_single.cpp
@@ -20,6 +20,7 @@ int main(int argc, char** argv)
   std::string sceneFile = argv[1];
   std::vector<Sphere*> scene_spheres = shapeIO.load_scene(sceneFile);
 
+    camera cam = camera::getDefault();
     // Image
     const auto aspect_ratio = 3.0 / 2.0;
     const int image_width = 100;
@@ -30,12 +31,6 @@ int main(int argc, char** argv)
   // World
   BVH world(scene_spheres);
 
-  point3 lookfrom(13, 2, 3);
-  point3 lookat(0, 0, 0);
-  vec3 vup(0, 1, 0);
-  auto dist_to_focus = 10.0;
-  auto aperture = 0.1;
-  camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
   traceConfig config(cam, image_width, image_height, max_depth, samples_per_pixel, 1, 0, 1);
 
