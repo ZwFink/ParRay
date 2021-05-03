d2d51403b15bf20243dad82076db8c97c2964964
Harry <cjf12harry@gmail.com>
2021-05-03 08:51:40 -0400

    add tile decomposition (#24)

--- src/benchmark/benchmark_bvh.cpp
+++ src/benchmark/benchmark_bvh.cpp
@@ -53,8 +53,31 @@ static void BM_Baseline_BVH_Tracing_at_sceneSize(benchmark::State &state)
 }
 BENCHMARK(BM_Baseline_BVH_Tracing_at_sceneSize)->Unit(benchmark::kMillisecond)->RangeMultiplier(2)->Range(1,10);
 
+static void BM_BVH_Tracing_OMP_threadsNum_tileSize(benchmark::State &state)
+{
+    int num_thread=state.range(0);
+    int tileSize = state.range(1);
+    ShapeDataIO io;
 
+    camera cam = camera::getDefault();
+    // Image
+    const int image_width = 1200;
+    const int image_height = static_cast<int>(image_width / cam.aspect_ratio);
+    const int samples_per_pixel = 10;
+    const int max_depth = 5;
+    //create scene
+    SphereGeneration sphereGen;
+    std::vector<Sphere*> spheres = sphereGen.random_scene_Spheres(5);
+    BVH world(spheres);
+    //create config
+    traceConfig config(cam, image_width, image_height, max_depth, samples_per_pixel, num_thread,0,1);
 
+    for (auto _ : state){
+     raytracing_bvh_tiled(config,world,tileSize);
+    }
+    io.clear_scene(spheres);
+}
+BENCHMARK(BM_BVH_Tracing_OMP_threadsNum_tileSize)->Unit(benchmark::kMillisecond)->RangeMultiplier(2)->Ranges({{1, 8}, {1, 64}});
 
 static void BM_BVH_Tracing_at_threadNum(benchmark::State &state)
 {
@@ -63,10 +86,10 @@ static void BM_BVH_Tracing_at_threadNum(benchmark::State &state)
 
     camera cam = camera::getDefault();
     // Image
-    const int image_width = 100;
+    const int image_width = 1200;
     const int image_height = static_cast<int>(image_width / cam.aspect_ratio);
-    const int samples_per_pixel = 100;
-    const int max_depth = 3;
+    const int samples_per_pixel = 10;
+    const int max_depth = 5;
     BVH world(sceneSpheres);
 
     int num_threads = state.range(0);
